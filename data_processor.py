0ac63c6c8ce7c557049f9afee28a5a0829dbd4fe
Joseph Ananda <josephananda88888@gmail.com>
2021-11-17 14:57:57 +0700

    Initial commit

---
+++ data_processor.py
@@ -0,0 +1,21 @@
+import torch
+import torch.nn.functional as F
+from nltk.tokenize import word_tokenize
+
+def predict(text, model, max_len, vocab):
+    """Memprediksi probabilitas seorang penulis (teks) ini introvert."""
+
+    # Melakukan tokenisasi, padding, dan encoding pada teks
+    tokens = word_tokenize(text.lower())
+    padded_tokens = tokens + ['<pad>'] * (max_len - len(tokens))
+    input_id = [vocab.get(token, vocab['<unk>']) for token in padded_tokens]
+
+    # Konversi ke PyTorch tensors
+    input_id = torch.tensor(input_id).unsqueeze(dim=0)
+
+    # Menghitung logits (layer akhir yang akan dimasukkan ke dalam softmax)
+    logits = model.forward(input_id)
+
+    # Menghitung probabilitas
+    probs = F.softmax(logits, dim=1).squeeze(dim=0)
+    return probs[0] * 100, probs[1] * 100
---
+++ main.py
@@ -0,0 +1,43 @@
+import streamlit as st
+import torch
+import json
+import data_processor
+
+def predict(text):
+    model_ei = torch.load("model/modelei.pth")
+    model_ns = torch.load("model/modelns.pth")
+    model_ft = torch.load("model/modelft.pth")
+    model_jp = torch.load("model/modeljp.pth")
+    with open('word_index.json') as json_file:
+        vocab = json.load(json_file)
+
+    extrovert, introvert = data_processor.predict(text, model_ei, 50, vocab)
+    return extrovert, introvert
+
+if __name__ == '__main__':
+    # giving the webpage a title
+    st.title("MBTI Type Prediction")
+
+    # here we define some of the front end elements of the web page like
+    # the font and background color, the padding and the text to be displayed
+    html_temp = """
+    <div style ="background-color:blue;padding:13px">
+    <h1 style ="color:black;text-align:center;">MBTI Type Classifier</h1>
+    </div>
+    """
+
+    # this line allows us to display the front end aspects we have
+    # defined in the above code
+    st.markdown(html_temp, unsafe_allow_html=True)
+
+    # the following lines create text boxes in which the user can enter
+    # the data required to make the prediction
+    text = st.text_input("Insert Any Text", "")
+    result = ""
+
+    # the below line ensures that when the button called 'Predict' is clicked,
+    # the prediction function defined above is called to make the prediction
+    # and store it in the variable result
+    if st.button("Predict"):
+        extr, intr = predict(text)
+    st.success('The output is {}, {}'.format(extr, intr))
